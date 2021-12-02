63f3d35dcf0befebd0cc364d230d687147a733f0
Dzides <idzigun@.ru>
2021-12-02 22:52:04 +0300

    added mobil menu $ scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(function () {
 
+    $('.header__burger').on('click', function () {
+       $('.header__burger').toggleClass('header__burger--active');
+       $('.header__menu').toggleClass('header__menu--active');
+       $('body').toggleClass('lock');
+
+    });
+
+    $('.header__link').on('click', function () {
+       $('.header__burger').removeClass('header__burger--active');
+       $('.header__menu').removeClass('header__menu--active');
+
+    });
+
+    $(window).scroll(function () {
+       if ($(document).scrollTop() > 100) {
+          $(".header__top").addClass("header__top--animate");
+       } else {
+          $(".header__top").removeClass("header__top--animate");
+       }
+    });
+
    var mixer = mixitup('.portfolio__board');
 
 });
