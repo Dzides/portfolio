3097d826506230bd62ac2f4423e78888a8cad7f0
Dzides <idzigun@.ru>
2022-01-29 21:16:00 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
 $(function () {
 
-    $('.burger').on('click', function () {
-       $('.burger').toggleClass('burger--active');
-       $('.menu').toggleClass('menu--active');
-       $('body').toggleClass('lock');
+   $('.burger').on('click', function () {
+      $('.burger').toggleClass('burger--active');
+      $('.menu').toggleClass('menu--active');
+      $('body').toggleClass('lock');
 
-    });
+   });
 
-    $('.menu__link').on('click', function () {
-       $('.burger').removeClass('burger--active');
-       $('.menu').removeClass('menu--active');
-       $('body').removeClass('lock');
+   $('.menu__link').on('click', function () {
+      $('.burger').removeClass('burger--active');
+      $('.menu').removeClass('menu--active');
+      $('body').removeClass('lock');
 
-    });
+   });
 
-    $(window).scroll(function () {
-       if ($(document).scrollTop() > 100) {
-          $(".header__nav").addClass("header__nav--animate");
-       } else {
-          $(".header__nav").removeClass("header__nav--animate");
-       }
-    });
+   $(window).scroll(function () {
+      if ($(document).scrollTop() > 100) {
+         $(".header__wrapper").addClass("header__wrapper--animate");
+         $(".return").addClass("return__animate");
+         $(".header__nav").addClass("header__nav--animate");
+      } else {
+         $(".header__wrapper").removeClass("header__wrapper--animate");
+         $(".return").removeClass("return__animate");
+         $(".header__nav").removeClass("header__nav--animate");
+      }
+   });
+
+   $(".menu__link, .logo, .header__link, .return").on("click", function (event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),
+         top = $(id).offset().top;
+      $('body,html').animate({
+         scrollTop: top
+      }, 1500);
+   });
 
    var mixer = mixitup('.portfolio__board');
 
